2a8c49c93127d6cacc812689c1545d87c832b1c9
Dalton <daltonamurray@proton.me>
2025-09-04 18:05:31 -0500

    Final RestaurantOrderMethodsLab.java
    The final version of the lab that will be submitted.

--- RestaurantOrderMethodsLab.java
+++ RestaurantOrderMethodsLab.java
@@ -14,7 +14,27 @@ public class RestaurantOrder {
     // Method to get the price of an item based on selection
     public static double getItemPrice(int itemNumber) {
         // TODO: Implement logic to return the price based on itemNumber
-        return 0.0;
+        double itemPrice;
+
+        if (itemNumber == 1)
+        {
+            itemPrice = 5.99;
+        }
+        else if (itemNumber == 2)
+        {
+            itemPrice = 8.99;
+        }
+        else if (itemNumber == 3)
+        {
+            itemPrice = 4.99;
+        }
+        else if (itemNumber == 4)
+        {
+            itemPrice = 1.99;
+        }
+        else itemPrice = 0.0;
+        
+        return itemPrice;
     }
  
     // Method to take an order from the user
@@ -27,21 +47,38 @@ public class RestaurantOrder {
         while (true) {
             int choice = scanner.nextInt();
             if (choice == 0) break;
-            
+            else if (choice >= 1 && choice <= 4)
+                totalCost = totalCost + getItemPrice(choice);
             // TODO: Call getItemPrice() and update totalCost
         }
  
         // TODO: Display total cost
+       System.out.println("Calculating total now...");
+       totalCost = calculateTotal(totalCost);
+       System.out.println("Your total is: " + totalCost + ".");
     }
  
     // Overloaded method to apply a discount
     public static double calculateTotal(double totalCost) {
         // TODO: Implement method to return total cost
-        return totalCost;
+       Scanner scanner = new Scanner(System.in);
+         System.out.println("Do you plan to use a discount? (True for yes, False for no): ");
+        boolean isDiscount = scanner.nextBoolean();
+        if (isDiscount == true)
+        {
+            totalCost = calculateTotal(totalCost, 0.15);
+        }
+        else if (isDiscount == false)
+        {
+            totalCost = calculateTotal(totalCost, 0.0);
+        }
+       return totalCost;
     }
  
     public static double calculateTotal(double totalCost, double discount) {
         // TODO: Implement method to return total cost after discount
+        double discountCost = (totalCost * discount);
+            totalCost = totalCost - discountCost;
         return totalCost;
     }
  
